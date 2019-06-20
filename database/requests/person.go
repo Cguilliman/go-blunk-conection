a2de8700b98c86935eecc52b298df1221ac752b0
cguilliman <cguilliman@gmail.com>
2019-06-20 18:06:31 +0300

    start person creation

--- database/requests/person.go
+++ database/requests/person.go
@@ -1,1 +1,47 @@
 package requests
+
+import (
+    "fmt"
+    "reflect"
+    "github.com/Cguilliman/test_blunk_db/database/models"
+    "github.com/Cguilliman/test_blunk_db/database/base"
+)
+
+// get current user
+func GetPerson(id int) {
+    database := base.GetDB()
+    person := new(models.Person)
+    err := database.QueryRow(fmt.Sprintf(`
+        select 
+            Person.ID, Person.Username, 
+            Person.FirstName, Person.LastName 
+        from Person 
+        where Person.ID=%d
+    `, id)).Scan(
+        &person.ID, &person.Username, 
+        &person.LastName, &person.FirstName,
+    )
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(person)
+}
+
+// create person (registration)
+func CreatePerson(person *models.Person) {
+    elems := reflect.ValueOf(person).Elem()
+    typeOf := elems.Type()
+
+    for i := 0; i < elems.NumField(); i++ {
+        fmt.Println(typeOf.Field(i).Name)
+        fmt.Println(elems.Field(i))
+    }
+    // database := base.GetDB()
+    // rows, err := database.Query(`
+    //     insert into Person ()
+    // `)
+}
+
+// update person
+func UpdatePerson() {
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     // "database/sql"
     "github.com/gin-gonic/gin"
     "github.com/Cguilliman/test_blunk_db/database/requests"
+    "github.com/Cguilliman/test_blunk_db/database/models"
     db "github.com/Cguilliman/test_blunk_db/database/base"
 )
 
@@ -15,10 +16,16 @@ func main() {
     // requests.GetRooms()
     // requests.MainUserRoomsList(1)
 
-    requests.GetMessages(1, 2)
-    requests.GetMessages(2, 2)
-    requests.GetMessages(3, 2)
+    // requests.GetMessages(1, 2)
+    // requests.GetMessages(2, 2)
+    // requests.GetMessages(3, 2)
     
+    // requests.GetPerson(1)
+    requests.CreatePerson(&models.Person{
+        Username:  "James",
+        FirstName: "fName",
+        LastName:  "lName",
+    })
     engine := gin.Default()
     engine.Run("0.0.0.0:8000")
 }
